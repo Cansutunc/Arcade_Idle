531bde6b7dc77d210d298cd3ef1ff252e0b1c5a5
Cansu Tunç <cansutunc00@gmail.com>
2022-08-14 17:43:47 +0300

    StackArea&Camera

--- Assets/Scripts/StackMechanic.cs
+++ Assets/Scripts/StackMechanic.cs
@@ -5,26 +5,50 @@ using DG.Tweening;
 
 public class StackMechanic : MonoBehaviour
 {
-    public Transform Itemholdertransform;
+    public Transform itemHolderTransform;
     public int numofitemholding=0;
-    List<int> ItemlistNum = new List<int>();
+    public List <Transform> listOfitems;
+    private Transform currentDropArea;
+    private bool isDroppable =true;
+
     public void AddNewItem(Transform _ItemtoAdd)
     {
-        _ItemtoAdd.DOJump(Itemholdertransform.position +new Vector3(0, 0.025f * numofitemholding, 0),1.5f,1,0.25f).OnComplete(
+        listOfitems.Add(_ItemtoAdd);
+        _ItemtoAdd.DOJump(itemHolderTransform.position +new Vector3(0, 0.025f * numofitemholding, 0),1.5f,1,0.25f).OnComplete(
             () =>
             {
-                _ItemtoAdd.SetParent(Itemholdertransform, true);
+                _ItemtoAdd.SetParent(itemHolderTransform, true);
                 _ItemtoAdd.localPosition = new Vector3(0,0.25f*numofitemholding,0);
                 _ItemtoAdd.localRotation = Quaternion.identity;
-                numofitemholding++;
-                if (numofitemholding > 9)
-                {
-                    
-                    
-                }
-
+                numofitemholding++;             
             }
-
             );
     }   
+    public void DropCollectedItem()
+    {
+        isDroppable = false;
+        Transform itemDrop;
+        //1 sn sonra callu yap
+        DOVirtual.DelayedCall(1f,() =>
+        {
+            isDroppable = true;
+        });
+        if(listOfitems.Count == 0)
+        {
+            return;
+        }
+        itemDrop = listOfitems[listOfitems.Count-1];
+        listOfitems.Remove(itemDrop);
+        itemDrop.SetParent(currentDropArea, true);
+        itemDrop.DOMove(currentDropArea.position, 0.5f);
+
+    }
+    private void OnTriggerStay(Collider other)
+    {        
+        if (other.CompareTag("DropArea")&& isDroppable)
+        {
+            currentDropArea = other.GetComponent<Transform>();
+            DropCollectedItem();            
+        }
+    }
 }
